7c55dc60872dc643c9628afa436bee054358a57a
sydneyIdundun <sydney.idundun@gmail.com>
2023-04-04 15:27:47 +0400

    Fix: Fixed the issue with using provider on the splash screen :bug:

--- medigo_app/lib/screens/SplashScreen.dart
+++ medigo_app/lib/screens/SplashScreen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:medigo_app/components/customWidgets.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.white),
+          child: Center(child: const InputField()),
+        ),
+      ),
+    );
   }
 }
