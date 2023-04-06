3a5afa3ad7ca7459ca51ee71e5cd63776e1beab4
Harith Onigemo <onigemotosin@gmail.com>
2023-04-06 06:00:53 +0400

    feat: Implemented login and register pages:sparkles:

--- medigo_app/lib/services/NavigationProvider.dart
+++ medigo_app/lib/services/NavigationProvider.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class NavigationProvider extends ChangeNotifier {
   String _currentRoute = "home"; // current route default set to home
+  // Define route names as constants
+  late String _previousRoute;
 
   String get currentRoute => _currentRoute;
+  String get previousRoute => _previousRoute;
 
   void navigateTo(String route) {
     // Method to change route
+    _previousRoute = _currentRoute;
     _currentRoute = route;
+
+    notifyListeners();
+  }
+
+  void goBack(BuildContext context) {
+    Navigator.pop(context);
     notifyListeners();
   }
 }
