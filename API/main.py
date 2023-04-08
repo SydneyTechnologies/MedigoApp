f38f35075621304c5d88e6478bd5dc7f7db4797c
sydneyIdundun <sydney.idundun@gmail.com>
2023-04-09 03:41:01 +0400

    setting up the development environment for this project
    environment variables for mongodb atlas collection

--- API/main.py
+++ API/main.py
@@ -1,2 +1,19 @@
 # first import the fastapi library
 from fastapi import FastAPI
+from models import  *
+
+import os
+import pymongo
+import certifi
+
+app = FastAPI()
+database_url = os.environ.get("DATABASE_URL")
+db_client = pymongo.MongoClient(database_url, tlsCAFile = certifi.where())
+
+# an endpoint to list all the medications 
+@app.get("/medications", response_model=Medication)
+def list_medications():
+    return
+
+
+
